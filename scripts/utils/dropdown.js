0234e962e02ecf520591ef525a45c3580b2586f0
Novae <cedricboutillier59@gmail.com>
2022-12-18 02:42:29 +0100

    Added listener to dropdown items, ability to display and remove tags, input displaying when dropdown is open

--- scripts/utils/dropdown.js
+++ scripts/utils/dropdown.js
@@ -4,23 +4,57 @@ const wrapperUstensils = document.querySelector('.wrapper-ustensils');
 const dropdownIngredients = document.querySelector('.dropdown-ingredients');
 const dropdownDevices = document.querySelector('.dropdown-devices');
 const dropdownUstensils = document.querySelector('.dropdown-ustensils');
+const tags = document.querySelector( '.tags' );
+//Ajout des ecouteurs d'evenements sur les dropdowns
 
-function openDropdown(event) {
+function toggleDropdown(event) {
     const parent = event.target.parentNode;
     parent.classList.toggle('wrapper-dropdown-open');
 }
 
-wrapperIngredient.addEventListener('click', (event) => {
-    openDropdown(event);
-});
+function clickedDropdownItem(event){
+    const tag = document.createElement( 'div' );
+    const nameTag = document.createElement( 'span' );
+    const closeImg = document.createElement( 'img' );
 
-wrapperDevices.addEventListener('click', (event) => {
-    openDropdown(event);
-})
+    nameTag.textContent = event.target.textContent;
+    closeImg.setAttribute('src', './assets/images/icon-close-tag.svg');
+    closeImg.classList.add('tag-close-icon');
 
-wrapperUstensils.addEventListener('click', (event) => {
-    openDropdown(event);
-})
+    tag.classList.add('tag');
+    tag.classList.add('tag-ingredient');
+
+    tag.appendChild(nameTag);
+    tag.appendChild(closeImg);
+
+    tags.appendChild(tag);
+    tags.style.display = 'flex';
+    closeImg.addEventListener('click', (event) => {
+        closeTag(event.target);
+    })
+}
+
+function addListener(node){
+    if(node.hasChildNodes()){
+        for(const child of node.children){
+            if(child.tagName != 'UL' && child.tagName != 'INPUT'){
+                child.addEventListener('click', (event) => {
+                    toggleDropdown(event);
+                })
+            }
+            if(child.tagName == 'UL'){
+                child.addEventListener('click', (event) => {
+                    clickedDropdownItem(event);
+
+                })
+            }
+        }
+    }
+}
+addListener(wrapperIngredient);
+addListener(wrapperDevices);
+addListener(wrapperUstensils);
+
 
 //Affichage des menus dropdowns
 function displayFilters(recipes) {
@@ -37,6 +71,7 @@ function displayFilters(recipes) {
         displayUstensilsFilter(ustensils);
     })
 }
+
 
 function displayIngredientsFilter(ingredients) {
     ingredients.forEach((ingredient) => {
