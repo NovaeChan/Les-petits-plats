32b27c8c42c2075b525b03dca32204540c2a75a0
NovaeChan <novaestar@protonmail.com>
2023-01-23 15:26:20 +0100

    Fixed an issue when researching a recipe when a tag has already been checked

--- scripts/utils/searchBar.js
+++ scripts/utils/searchBar.js
@@ -1,26 +1,23 @@
-const searchBar = document.getElementById('recipe-form');
+const searchBar = document.getElementById("recipe-form");
 
 //Function rechercher ingredient, nom de recette, description
-function searchInput(inputValue, recipes){
+function searchInput(inputValue, recipes) {
     console.time();
-    const regex = new RegExp(inputValue, "i");
+    // const regex = new RegExp(inputValue, "i");
     let filterItems = [];
-    for(let i = 0; i < recipes.length; i++){
+    for (let i = 0; i < recipes.length; i++) {
         let recipe = recipes[i];
-        if(regex.test(recipe.name) && !filterItems.includes(recipe)){
+        if (recipe.name.toLowerCase().trim().includes(inputValue.toLowerCase().trim()) && !filterItems.includes(recipe)) {
             filterItems.push(recipe);
             continue;
         }
-        if(regex.test(recipe.appliance) && !filterItems.includes(recipe)){
+        if (recipe.description.toLowerCase().trim().includes(inputValue.toLowerCase().trim()) && !filterItems.includes(recipe)) {
             filterItems.push(recipe);
             continue;
         }
-        if(regex.test(recipe.description) && !filterItems.includes(recipe)){
-            filterItems.push(recipe);
-            continue;
-        }
-        for(let i = 0; i < recipe.ingredients.length; i++){
-            if(regex.test(recipe.ingredients[i].ingredient) && !filterItems.includes(recipe)){
+        for (let i = 0; i < recipe.ingredients.length; i++) {
+            if (recipe.ingredients[i].ingredient.toLowerCase().trim().includes(inputValue.toLowerCase().trim()) 
+            && !filterItems.includes(recipe)) {
                 filterItems.push(recipe);
                 break;
             }
@@ -31,11 +28,11 @@ function searchInput(inputValue, recipes){
     return searchedRecipes;
 }
 
-function searchInputFromUstensils(inputValue, recipes){
-    const regex = new RegExp(`${inputValue}`, "i");
+function searchInputFromUstensils(inputValue, recipes) {
+    // const regex = new RegExp(`${inputValue}`, "i");
     let filterUstensil = [];
-    for(let i = 0; i < recipes.length; i++){
-        if(regex.test(recipes[i].ustensils) && !filterUstensil.includes(recipes[i])){
+    for (let i = 0; i < recipes.length; i++) {
+        if ( recipes[i].ustensils.includes(inputValue) && !filterUstensil.includes(recipes[i])) {
             filterUstensil.push(recipes[i]);
         }
     }
@@ -43,36 +40,122 @@ function searchInputFromUstensils(inputValue, recipes){
     return searchedRecipes;
 }
 
-function displaySearchInputFromUstensils(inputValue, recipes){
+function displaySearchInputFromUstensils(inputValue, recipes) {
     const searchedRecipes = searchInputFromUstensils(inputValue, recipes);
     searchedRecipes.length > 0 ? displayRecipes(searchedRecipes) : displayNoRecipes();
     displayFilters(searchedRecipes);
 }
 
 //Faire une deuxième fonction pour les dropdowns
-function displaySearchInput(inputValue, recipes){
+function displaySearchInput(inputValue, recipes) {
     const searchedRecipes = searchInput(inputValue, recipes);
     //S'il y a des recettes trouvées alors on les affiche sinon message d'erreur
-    searchedRecipes.length > 0 ? displayRecipes(searchedRecipes) : displayNoRecipes();
+    searchedRecipes.length > 0
+        ? displayRecipes(searchedRecipes)
+        : displayNoRecipes();
     //Faire en sorte que les menus dropdowns soient mis à jour
     displayFilters(searchedRecipes);
 }
 
-searchBar.addEventListener('input', (event) => {
+searchBar.addEventListener("input", (event) => {
     if (event.currentTarget.value.length >= 3) {
         const searchedItem = event.currentTarget.value.trim().toLowerCase();
-        displaySearchInput(searchedItem, recipes);
+        if(selectedTags.length <= 0){
+            displaySearchInput(searchedItem, recipes);
+        }
+        else{
+            const tagUstensils = document.querySelectorAll('.tag-ustensils');
+            if(tagUstensils.length > 0){
+                let ustensilsTags = [];
+                tagUstensils.forEach((tag) => {
+                    ustensilsTags.push(tag.querySelector('span').innerHTML);
+                })
+                for(let i = 0; i < selectedTags.length; i++){
+                    if(ustensilsTags.includes(selectedTags[i])){
+                        if(i == 0){
+                            displaySearchInputFromUstensils(selectedTags[0], recipes);
+                            continue;
+                        }
+                        displaySearchInputFromUstensils(selectedTags[i], searchedRecipes);
+                        continue;
+                    }
+                    if( i == 0){
+                        displaySearchInput(selectedTags[i], recipes);
+                        continue;
+                    }
+                    displaySearchInput(selectedTags[i], searchedRecipes);
+                }
+                displaySearchInput(searchedItem, searchedRecipes);
+            }
+            else {
+                for(let i = 0; i < selectedTags.length; i++){
+                    if(i === 0 ){
+                        displaySearchInput(selectedTags[i], recipes);
+                        continue;
+                    }
+                    displaySearchInput(selectedTags[i], searchedRecipes);
+                }
+                displaySearchInput(searchedItem, searchedRecipes);
+            }
+        }
     }
 });
 
-searchBar.addEventListener('keyup', (event) => {
-    if(event.key == 'Backspace' || event.key == 'Delete'){
+searchBar.addEventListener("keyup", (event) => {
+    if (event.key == "Backspace" || event.key == "Delete") {
         const searchedItem = event.currentTarget.value.trim().toLowerCase();
         if( searchedItem.length < 3 ){
             searchedRecipes = [];
-            displayRecipes(recipes);
-            displayFilters(recipes);
+            if(selectedTags.length <= 0){
+                displayRecipes(recipes);
+                displayFilters(recipes);
+            }
+            else{
+                const tagUstensils = document.querySelectorAll('.tag-ustensils');
+                if(tagUstensils.length > 0){
+                    let ustensilsTags = [];
+                    tagUstensils.forEach((tag) => {
+                        ustensilsTags.push(tag.querySelector('span').innerHTML);
+                    })
+                    for(let i = 0; i < selectedTags.length; i++){
+                        if(ustensilsTags.includes(selectedTags[i])){
+                            if( i === 0 ){
+                                displaySearchInputFromUstensils(selectedTags[i], recipes);
+                                continue;
+                            }
+                            displaySearchInputFromUstensils(selectedTags[i], searchedRecipes);
+                            continue;
+                        }
+                        if(i === 0 ){
+                            displaySearchInput(selectedTags[i], recipes);
+                            continue;
+                        }
+                        displaySearchInput(selectedTags[i], searchedRecipes);
+                    }
+                }
+                else {
+                    for(let i = 0; i < selectedTags.length; i++){
+                        if(i === 0 ){
+                            displaySearchInput(selectedTags[i], recipes);
+                            continue;
+                        }
+                        displaySearchInput(selectedTags[i], searchedRecipes);
+                    }  
+                }
+            }
         }
     }
-})
+});
 
+/* 
+
+Salade de riz
+Tarte aux pommes 
+Salade machée 
+Crepes chocolat 
+cookies 
+crumble aux pommes
+charlottes au poires
+creme patissière
+
+*/
--- scripts/utils/tags.js
+++ scripts/utils/tags.js
@@ -16,28 +16,78 @@ function removeSelectedTag(tag){
 
 //Ajouter une vérification pour savoir si une recherche a été effectuée
 function getRecipeWithTags(){
-    console.log(selectedTags);
     const inputValue = document.querySelector('#recipe-form').value;
     if(inputValue.length > 2){
-        if(selectedTags.length != 0){
+        if(selectedTags <= 0){
+            displaySearchInput(inputValue, recipes);
+        }
+        else{
+            const tagUstensils = documen.querySelectorAll('.tag-ustensils');
+            if(tagUstensils.length > 0){
+                let ustensilsTags = [];
+                tagUstensils.forEach(tag => {
+                    ustensilsTags.push(tag.querySelector('span').innerHTML)
+                });
+                for(let i = 0; i < selectedTags.length; i++){
+                    if(ustensilsTags.includes(selectedTags[i])){
+                        if( i === 0){
+                            displaySearchInputFromUstensils(selectedTags[0], recipes);
+                            continue;
+                        }
+                        displaySearchInputFromUstensils(selectedTags[i], searchedRecipes);
+                        continue;
+                    }
+                    if( i === 0){
+                        displaySearchInput(selectedTags[0], recipes);
+                        continue;
+                    }
+                    displaySearchInput(selectedTags[i], searchedRecipes);
+                }
+                displaySearchInput(inputValue, searchedRecipes);
+            }
+            else {
+                for(let i = 0; i < selectedTags.length; i++){
+                    if( i === 0){
+                        displaySearchInput(selectedTags[i], recipes);
+                        continue;
+                    }
+                    displaySearchInput(selectedTags[i], searchedRecipes);
+                }
+                displaySearchInput(inputValue, searchedRecipes);
+            }
+        }
+    }
+    else if(selectedTags.length > 0){
+        const tagUstensils = document.querySelectorAll('.tag-ustensils');
+        if(tagUstensils.length > 0){
+            let ustensilsTags = [];
+            tagUstensils.forEach((tag) => {
+                ustensilsTags.push(tag.querySelector('span').innerHTML);
+            })
             for(let i = 0; i < selectedTags.length; i++){
+                if(ustensilsTags.includes(selectedTags[i])){
+                    if( i === 0){
+                        displaySearchInputFromUstensils(selectedTags[0], recipes);
+                        continue;
+                    }
+                    displaySearchInputFromUstensils(selectedTags[i], searchedRecipes);
+                    continue;
+                }
                 if( i === 0){
-                    displaySearchInput(inputValue, recipes);
+                    displaySearchInput(selectedTags[0], recipes);
+                    continue;
                 }
                 displaySearchInput(selectedTags[i], searchedRecipes);
             }
         }
-        else{
-            displaySearchInput(inputValue, recipes);
-        }
-    }
-    else if(selectedTags.length > 0){
-        for(let i = 0; i < selectedTags.length; i++){
-            if(i === 0){
-                displaySearchInput(selectedTags[i], recipes);
-                continue;
+        else {
+            for(let i = 0; i < selectedTags.length; i++){
+                if(i === 0){
+                    displaySearchInput(selectedTags[i], recipes);
+                    continue;
+                }
+                displaySearchInput(selectedTags[i], searchedRecipes);
             }
-            displaySearchInput(selectedTags[i], searchedRecipes);
         }
     }
     else{
